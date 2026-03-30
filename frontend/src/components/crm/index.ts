6372527d0407e7f0436046d9f64114e75cf98f77
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2026-03-30 14:13:53 -0500

    feat(crm): add barrel export for CRM components
    - Centralized exports for all CRM sub-components
    - Includes type exports for LeadFormData

---
+++ frontend/src/components/crm/index.ts
@@ -0,0 +1,14 @@
+/**
+ * CRM Components - Barrel Export
+ * Exportaciones centralizadas de componentes CRM
+ *
+ * @module components/crm
+ */
+
+export { default as KanbanBoard } from './KanbanBoard';
+export { default as LeadCard } from './LeadCard';
+export { default as LeadModal } from './LeadModal';
+export type { LeadFormData } from './LeadModal';
+export { initialLeadFormData } from './LeadModal';
+export { default as TaskCard } from './TaskCard';
+export { default as TaskModal } from './TaskModal';
