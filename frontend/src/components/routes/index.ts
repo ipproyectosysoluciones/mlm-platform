f9b71d75f999b46c2e57eb39c0e34142b13273d8
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2026-03-30 14:34:15 -0500

    feat(routes): add barrel export for route guard components

---
+++ frontend/src/components/routes/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview Route guard components
+ * @description Centralized exports for route protection components
+ * @module components/routes
+ */
+
+export { ProtectedRoute } from './ProtectedRoute';
+export { AdminRoute } from './AdminRoute';
+export { PublicRoute } from './PublicRoute';
+export { PublicProfileRoute } from './PublicProfileRoute';
